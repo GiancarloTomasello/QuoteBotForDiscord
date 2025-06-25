092f5f65475c3352749eb1c288df29c6c685044c
Giancarlo Tomasello <gtomasel20@gmail.com>
2025-06-25 13:16:20 -0700

    Tweaks to print the author of the quote & print errors when adding quote

--- index.js
+++ index.js
@@ -37,19 +37,23 @@ client.on('messageCreate', async(message) => {
 
     //split string to edit
     const command = message.content.split('"');
-    console.log(command);
-    console.log(command[0].toLowerCase());
 
+    if(command[0].slice(0,6).toLowerCase() == "!quote"){
+        if(message.channelId != process.env.QUOTE_CHANNEL_ID){
+            message.reply("Oops wrong channel. Please use the quote bot channel instead.");
+            return;
+        }
 
-    if(message.content.toLowerCase() == '!quote'){
         const response = await getQuote()
             .then( response => {
                 console.log(response);
-                message.reply(`${response.quote}`)
-                client.channels.cache.get(`${process.env.QUOTE_CHANNEL_ID}`).send(`${response.quote}`);
+
+                const chatMessage = response.author ? `${response.quote} ${response.author}` : `${response.quote}`
+                message.reply(chatMessage)
+                //client.channels.cache.get(`${process.env.QUOTE_CHANNEL_ID}`).send(chatMessage);
             })
             .catch(error => {
-                console.log('Error fetching data: ', error);
+                console.log("Error fetching data: ", error);
             })
     }
 
@@ -57,9 +61,14 @@ client.on('messageCreate', async(message) => {
         console.log(message.content);
         console.log("Adding Quote To Database.....");
         const author = command.length == 3 ? command[command.length-1] : '';
-
-
-        await saveQuote({quote: command[1], author: author});
+        await saveQuote({quote: command[1], author: author})
+            .then(() => {
+                message.reply("Quote added to database");
+            })
+            .catch(error => {
+                message.reply("Error adding quote to database");
+                console.log("Unable to save quote: ", error);
+            });
     }
 
 });
